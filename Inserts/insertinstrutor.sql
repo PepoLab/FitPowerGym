9ad5c8385a2838d23edf2772b4748c0275a13886
Pedro <rockpedro331@gmail.com>
2025-11-29 11:05:38 -0300

    Inserts para popular as tabelas do banco de dados

---
+++ Inserts/insertinstrutor.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_instrutor (nome, especialidade, email, telefone) VALUES
+('Carlos Almeida', 'Musculação', 'carlos@fitpower.com', '11988887777'),
+('Juliana Santos', 'Treinamento Funcional', 'juliana@fitpower.com', '11995554444'),
+('Ricardo Mendes', 'Hipertrofia', 'ricardo@fitpower.com', '11991234567');
---
+++ Inserts/insertplanos.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_plano (nome, duracao_meses, valor) VALUES
+('Mensal', 1, 120.00),
+('Trimestral', 3, 330.00),
+('Semestral', 6, 600.00),
+('Anual', 12, 1100.00);
