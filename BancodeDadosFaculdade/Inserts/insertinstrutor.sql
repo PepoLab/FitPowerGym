008f81d9fd47b4c878ee1f292c7c330f90d874b1
Pedro <rockpedro331@gmail.com>
2025-11-29 11:02:22 -0300

    Delete BancodeDadosFaculdade directory

--- BancodeDadosFaculdade/Inserts/insertinstrutor.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tb_instrutor (nome, especialidade, email, telefone) VALUES
-('Carlos Almeida', 'Musculação', 'carlos@fitpower.com', '11988887777'),
-('Juliana Santos', 'Treinamento Funcional', 'juliana@fitpower.com', '11995554444'),
-('Ricardo Mendes', 'Hipertrofia', 'ricardo@fitpower.com', '11991234567');
--- BancodeDadosFaculdade/Inserts/insertplanos.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb_plano (nome, duracao_meses, valor) VALUES
-('Mensal', 1, 120.00),
-('Trimestral', 3, 330.00),
-('Semestral', 6, 600.00),
-('Anual', 12, 1100.00);
